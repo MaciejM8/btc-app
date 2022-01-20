4547b347d05e14fb527a05d6c79000ca52c40628
Maciej Musial <maciejart89@gmail.com>
2022-01-20 21:35:12 +0100

    using autoformatter

--- btc/src/components/ChartDate.js
+++ btc/src/components/ChartDate.js
@@ -1,54 +1,30 @@
-import React from 'react'
+import React from "react";
 
+const ChartDate = (props) => {
+  // trzeba zmienic tablice czas odcinajac od Value w dol z wylaczeniem value
+  // this.setState(prevState => {
+  //     return {foo: {...prevState.foo, a: "updated"}};
+  // });
 
+  return (
+    <div className="ChartDate">
+      <p>Chart starting date</p>
 
-const ChartDate = (props)=> {
-    
-    
-   
-    
-    // trzeba zmienic tablice czas odcinajac od Value w dol z wylaczeniem value
-                        // this.setState(prevState => {
-                        //     return {foo: {...prevState.foo, a: "updated"}};
-                        // });
+      <input
+        min="2009-01-03"
+        max={props.CurrentDate}
+        value={props.Value}
+        onChange={props.onChange}
+        onKeyDown={(e) => e.preventDefault}
+        className="InputDate"
+        type="date"
+      />
 
-    
-    
+      <button onClick={props.onClick} className="submitBtn">
+        Submit
+      </button>
+    </div>
+  );
+};
 
-        
-
-
-    
-     
-        
-
-        return (
-            
-            <div className="ChartDate">
-            <p>Chart starting date</p>
-            
-                          
-            <input
-
-                min='2009-01-03'
-                max={props.CurrentDate}
-                value={props.Value} 
-                onChange={props.onChange} 
-                onKeyDown={(e)=> e.preventDefault}className="InputDate" type="date" />
-            
-            <button 
-            
-            onClick={props.onClick}
-            className="submitBtn"
-            
-            
-            >Submit</button>
-
-            
-            
-
-            </div> );
-    }
-
- 
 export default ChartDate;
