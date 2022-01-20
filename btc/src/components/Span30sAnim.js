5b10b54722bb0a111d16ebba52e92dffde9b3f4e
Maciej Musial <maciejart89@gmail.com>
2022-01-20 21:32:31 +0100

    Auto format component

--- btc/src/components/Span30sAnim.js
+++ btc/src/components/Span30sAnim.js
@@ -1,63 +1,52 @@
-import React, { Component } from 'react';
-import '../styles/ChartComponentStyle.css'
+import React, { Component } from "react";
+import "../styles/ChartComponentStyle.css";
 
+class Span30sAnim extends Component {
+  state = {
+    counter: 0,
+    spanClass: "Loading",
+    spanLoaded: "Loading Loaded",
+    spanLoadingMiddle: "LoadingMiddle",
+    spanClassMiddle: "LoadingMiddle Loaded",
+  };
 
-class Span30sAnim extends Component{
-
-    state ={
+  increment = () => {
+    this.setState((prevState) => ({
+      counter: this.state.counter + 1,
+    }));
+    if (this.state.counter === 30) {
+      this.setState({
         counter: 0,
-        spanClass: "Loading",
-        spanLoaded: "Loading Loaded",
-        spanLoadingMiddle: "LoadingMiddle",
-        spanClassMiddle: "LoadingMiddle Loaded",
+      });
     }
+  };
 
+  componentDidMount() {
+    setInterval(this.increment, 1000);
+  }
 
-
-    increment = () => {
-        this.setState(prevState => ({
-          counter:  this.state.counter + 1
-        }))
-        if(this.state.counter === 30){
-          this.setState({
-            counter: 0
-          })
-        }
-      }
-
-    
-    
-
-    componentDidMount(){
-        
-        setInterval(this.increment, 1000)
-    }
-
-
-    render(){
-        const { counter, spanClass, spanLoaded, spanClassMiddle, spanLoadingMiddle } = this.state
-    return ( 
-        <>
+  render() {
+    const {
+      counter,
+      spanClass,
+      spanLoaded,
+      spanClassMiddle,
+      spanLoadingMiddle,
+    } = this.state;
+    return (
+      <>
         <div className="ContainerLoading">
-            
-            <span className={counter >= 5 ? spanLoaded : spanClass}></span>
-            <span className={counter >= 11 ? spanLoaded : spanClass}></span>
-            <span  className=
-            
-            {counter >= 17 ? spanClassMiddle  : spanLoadingMiddle }
-            
-            >
-
-              <p>{this.state.counter}</p>
-              
-              </span>
-            <span className={counter >= 23 ? spanLoaded : spanClass}></span>
-            <span className={counter >= 29 ? spanLoaded : spanClass}></span>
-            
+          <span className={counter >= 5 ? spanLoaded : spanClass}></span>
+          <span className={counter >= 11 ? spanLoaded : spanClass}></span>
+          <span className={counter >= 17 ? spanClassMiddle : spanLoadingMiddle}>
+            <p>{this.state.counter}</p>
+          </span>
+          <span className={counter >= 23 ? spanLoaded : spanClass}></span>
+          <span className={counter >= 29 ? spanLoaded : spanClass}></span>
         </div>
-    
-        </>
+      </>
     );
-}}
- 
+  }
+}
+
 export default Span30sAnim;
