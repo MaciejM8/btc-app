ef2673181d91e456981cbe307e9f7072332ed615
Maciej Musial <maciejart89@gmail.com>
2022-02-19 20:23:49 +0100

    security changes in app.py # api key excluded

--- app.py
+++ app.py
@@ -13,13 +13,12 @@ import math
 from werkzeug.datastructures import Headers
 
 
-
-
 app = Flask(__name__)
 
 
 class Config:
     SCHEDULER_API_ENABLED = True
+
 
 app.config.from_object(Config())
 scheduler = APScheduler()
@@ -27,7 +26,7 @@ scheduler = APScheduler()
 # CORS(app)
 
 
-API_KEY = '1wVcUagruE4P0O5VPrLoACLfb9e'
+API_KEY = ""
 cg = CoinGeckoAPI()
 
 
@@ -38,21 +37,20 @@ cg = CoinGeckoAPI()
 acAd = False
 while acAd == False:
     try:
-    
-    
+
         print('****aktywne adresy****')
         print('zbieranie danych')
-        
+
         activeAdresses = 'https://api.glassnode.com/v1/metrics/addresses/active_count'
         res = requests.get(activeAdresses,
-        params={'a': 'BTC', 'api_key': API_KEY, 's':'1578167920'})
+                           params={'a': 'BTC', 'api_key': API_KEY, 's': '1578167920'})
 
         df = pd.read_json(res.text, convert_dates=['t'])
         print('data i ilosc aktywnych adresow')
         timeactive = df['t'].tolist()
         activeAd = df['v'].tolist()
-        
-        print("OK! ",timeactive[0],activeAd[0])
+
+        print("OK! ", timeactive[0], activeAd[0])
         if(res.status_code == 200):
             print('udalo sie wydobyc dane')
             acAd = True
@@ -60,37 +58,31 @@ while acAd == False:
     except requests.ConnectionError as e:
         print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
         print(str(e))
-          
+
 # btc price
 btcPr = False
 while btcPr == False:
-    
+
     try:
         print('****btc price****')
         print('zbieranie danych')
 
-        
-        
-
-        BTCprice= 'https://api.glassnode.com/v1/metrics/market/price_usd_close'
+        BTCprice = 'https://api.glassnode.com/v1/metrics/market/price_usd_close'
         res2 = requests.get(BTCprice,
-        params={'a': 'BTC', 'api_key': API_KEY, 's':'1578167920'})
+                            params={'a': 'BTC', 'api_key': API_KEY, 's': '1578167920'})
 
         df2 = pd.read_json(res2.text, convert_dates=['t'])
         print('data i cena btc')
-        
-        
+
         time2 = df2['t'].tolist()
         stringifiedTime2 = []
         for i in time2:
-            
+
             stringifiedTime2.append(i.isoformat()[0:10])
 
         btc_price = df2['v'].tolist()
-        
 
-
-        print("OK! ",time2[0],btc_price[0])
+        print("OK! ", time2[0], btc_price[0])
         if(res2.status_code == 200):
             print('udalo sie wydobyc dane')
             btcPr = True
@@ -99,26 +91,26 @@ while btcPr == False:
     except requests.ConnectionError as e:
         print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
         print(str(e))
-         
-    
+
+
 # newadressees
 
 
 neAd = False
-while  neAd == False:
+while neAd == False:
     try:
         print('****new adresses****')
         print('zbieranie danych')
 
-        NewAd= 'https://api.glassnode.com/v1/metrics/addresses/new_non_zero_count'
+        NewAd = 'https://api.glassnode.com/v1/metrics/addresses/new_non_zero_count'
         res3 = requests.get(NewAd,
-        params={'a': 'BTC', 'api_key': API_KEY, 's':'1578167920'})
+                            params={'a': 'BTC', 'api_key': API_KEY, 's': '1578167920'})
 
         df3 = pd.read_json(res3.text, convert_dates=['t'])
         print('data i nowe adresy')
         time3 = df3['t'].tolist()
         noweAdresy = df3['v'].tolist()
-        print("OK! ",time3[0],noweAdresy[0])
+        print("OK! ", time3[0], noweAdresy[0])
         if(res3.status_code == 200):
             print('udalo sie wydobyc dane')
             neAd = True
@@ -131,14 +123,14 @@ while  neAd == False:
 
 #  fees rate
 fees = False
-while  fees == False:
+while fees == False:
     try:
         print('****fees rate****')
         print('zbieranie danych')
 
-        fees= 'https://api.glassnode.com/v1/metrics/fees/volume_sum'
+        fees = 'https://api.glassnode.com/v1/metrics/fees/volume_sum'
         res4 = requests.get(fees,
-        params={'a':'btc', 'api_key': API_KEY, 's':'1578167920'})
+                            params={'a': 'btc', 'api_key': API_KEY, 's': '1578167920'})
 
         df4 = pd.read_json(res4.text, convert_dates=['t'])
         print('data i nowe adresy')
@@ -156,26 +148,24 @@ while  fees == False:
         print(str(e))
         continue
 hashRate = False
-while  hashRate == False:
+while hashRate == False:
     try:
         print('****btc hash rate****')
         print('zbieranie danych')
 
-        hashRate= 'https://api.glassnode.com/v1/metrics/mining/hash_rate_mean'
+        hashRate = 'https://api.glassnode.com/v1/metrics/mining/hash_rate_mean'
         res5 = requests.get(hashRate,
-        params={'a': 'BTC', 'api_key': API_KEY, 's':'1578167920'})
+                            params={'a': 'BTC', 'api_key': API_KEY, 's': '1578167920'})
 
         listOfDict = res5.json()
-        hashrateValues=[]
+        hashrateValues = []
         for i in listOfDict:
             for v in i.values():
-                hashrateValues.append(v) 
-        
+                hashrateValues.append(v)
+
         withoutTime = hashrateValues[1::2]
-        print("OK! ",withoutTime[0])
-        
-       
-        
+        print("OK! ", withoutTime[0])
+
         if(res5.status_code == 200):
             print('udalo sie wydobyc dane')
             hashRate = True
@@ -189,22 +179,18 @@ while  hashRate == False:
 # https://api.alternative.me/fng/
 
 fng = False
-while  fng == False:
+while fng == False:
     try:
         print('****Fear and greed****')
         print('zbieranie danych')
 
-        fng= 'https://api.alternative.me/fng/'
+        fng = 'https://api.alternative.me/fng/'
         resFng = requests.get(fng)
 
-        result=resFng.json()
-         
-        
-        
+        result = resFng.json()
+
         print("OK! ")
-        
-       
-        
+
         if(resFng.status_code == 200):
             print('udalo sie wydobyc dane')
             fng = True
@@ -214,21 +200,23 @@ while  fng == False:
         print("ERROR , połączenie / odrzucenie / powiązane z jakością internetu.\n")
         print(str(e))
 
-#end pointy z danymi
+# end pointy z danymi
 
 # btc price tiker from coingecko
 
-@scheduler.task('interval', id='do_job_1', seconds=30, misfire_grace_time=900)
-@app.route('/ticker', methods = ['GET'])
-def job1():
-    return cg.get_price(ids='bitcoin', vs_currencies='usd',include_market_cap=True, include_24hr_vol=True, include_24hr_change=True,)
-{'bitcoin': {'usd': 3462.04}}
 
+@scheduler.task('interval', id='do_job_1', seconds=30, misfire_grace_time=900)
+@app.route('/ticker', methods=['GET'])
+def job1():
+    return cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap=True, include_24hr_vol=True, include_24hr_change=True,)
+
+
+{'bitcoin': {'usd': 3462.04}}
 
 
 # @app.route('/timestamp', methods = ['GET','POST'])
 # def TimestampFromReact():
-    
+
 #     content = request.get_data(as_text=True)
 #     global timestamp
 #     timestamp = str(content)
@@ -236,45 +224,39 @@ def job1():
 #     return timestamp
 
 
-
-
-
-
-
-
-
-
-
-@app.route('/fearandgreed', methods = ['GET'])
+@app.route('/fearandgreed', methods=['GET'])
 def FearAndGreed():
     return jsonify(result)
 
 
-
-@app.route('/time', methods = ['GET'])
+@app.route('/time', methods=['GET'])
 def Time():
     return jsonify(stringifiedTime2)
 
-@app.route('/hashrate', methods = ['GET'])
+
+@app.route('/hashrate', methods=['GET'])
 def BtcHashRate():
     return jsonify(withoutTime)
 
-@app.route('/activeAd', methods = ['GET'])
+
+@app.route('/activeAd', methods=['GET'])
 def ActiveAd():
     return jsonify(activeAd)
 
-@app.route('/btcprice', methods = ['GET'])
+
+@app.route('/btcprice', methods=['GET'])
 def BtcPrice():
     return jsonify(btc_price)
 
-@app.route('/feesrate', methods = ['GET'])
+
+@app.route('/feesrate', methods=['GET'])
 def BtcFeesRate():
     return jsonify(feesRateFloored)
 
-@app.route('/new-ad', methods = ['GET'])
+
+@app.route('/new-ad', methods=['GET'])
 def newAdresses():
     return jsonify(noweAdresy)
-
 
 
 scheduler.init_app(app)
@@ -282,8 +264,6 @@ scheduler.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 
 # $env:FLASK_ENV = "development"
